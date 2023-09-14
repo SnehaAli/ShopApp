ee7aaa06ed4b9eaac9a52c49cdf4075fb17cf786
SnehaAli <nehaakhatoon72@gmail.com>
2023-09-14 14:56:31 +0530

    Association Added

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,11 +13,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user
+  .createProduct({
     title: title,
     imageUrl: imageUrl,
     price: price,
-    description: description
+    description: description,
+    userId: req.user.id
   })
   .then(result => {
     console.log('Item Added');
@@ -34,8 +36,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-  .then(product => {
+  req.user.getProducts({where: {id: prodId} })
+  // Product.findByPk(prodId)
+  .then(products => {
+    const product = products[0];
     if(!product) {
       return res.redirect('/');
     }
@@ -92,7 +96,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll().then(products => {
+  req.user.getProducts()
+  .then(products => {
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
