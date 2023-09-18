78e21452fab04f7cac2f383138a12d1f5ea4d302
SnehaAli <nehaakhatoon72@gmail.com>
2023-09-18 12:38:07 +0530

    connected the code with mongoDb

--- util/database.js
+++ util/database.js
@@ -1,7 +1,33 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('node-complete', 'root', 'password', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
+const mongobd = require('mongodb');
+const MongoClient = mongobd.MongoClient;
 
-module.exports = sequelize;
+let _db;
+
+//connecting
+const mongoConnect = callback => {
+    MongoClient.connect(
+        'mongodb+srv://nehaakhatoon72:gGPKUGwHP1Y1nBO2@cluster0.5il7znj.mongodb.net/shop?retryWrites=true&w=majority&appName=AtlasApp'
+    ) 
+    //storing the connection to database ,keep on running
+    .then(client => {
+        console.log('Connected');
+        _db = client.db();
+        callback(client);
+    })
+    .catch(err => {
+        console.log(err);
+        throw err;
+    });
+};
+
+//return access to that connection database if it exist
+const getDb = () => {
+    if(_db) {
+        return _db;
+    }
+    throw 'No Database Found!';
+};
+
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
