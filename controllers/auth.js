9f426dd3c1869e76d51d45bfbf8113da7d623c91
SnehaAli <nehaakhatoon72@gmail.com>
2023-10-04 12:54:59 +0530

    Added validation for signUp page

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,9 +3,10 @@ const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 // const mailgen = require('mailgen');
-const { validationResult } = require('express-validator/check');
+const { validationResult } = require('express-validator');
 
 const User = require('../models/user');
+const { error } = require('console');
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -25,7 +26,12 @@ exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    errorMessage: message
+    errorMessage: message,
+    oldInput: {
+      email: '',
+      password: ''
+    },
+    validationErrors: []
   });
 };
 
@@ -39,18 +45,47 @@ exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    errorMessage: message
-  });
+    errorMessage: message,
+    oldInput: {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    },
+    validationErrors: []
+    });
 };
 
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+  const errors  = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(422)
+    .render('auth/login', {
+      path: '/login',
+      pageTitle: 'Login',
+      errorMessage: errors.array()[0].msg,
+      oldInput: { 
+        email: email,
+        password: password
+      },
+      validationErrors: errors.array()
+    });
+  }
   User.findOne({email: email})
     .then(user => {
       if(!user) {
-        req.flash('error','invalid email or password');
-        return res.redirect('/login');
+        return res.status(422)
+        .render('auth/login', {
+          path: '/login',
+          pageTitle: 'Login',
+          errorMessage: 'Invalid email or password',
+          oldInput: { 
+            email: email,
+            password: password
+          },
+          validationErrors: []
+        });
       }
       bcrypt.compare(password, user.password)
       .then(doMatch => {
@@ -62,8 +97,17 @@ exports.postLogin = (req, res, next) => {
             res.redirect('/');
           });
         }
-        req.flash('error','invalid email or password');
-        res.redirect('/login');
+        return res.status(422)
+        .render('auth/login', {
+          path: '/login',
+          pageTitle: 'Login',
+          errorMessage:  'Invalid email or password',
+          oldInput: { 
+            email: email,
+            password: password
+          },
+          validationErrors: []
+        });
       })
       .catch(err => {
         console.log(err);
@@ -85,16 +129,16 @@ exports.postSignup = (req, res, next) => {
     .render('auth/signup', {
       path: '/signup',
       pageTitle: 'Signup',
-      errorMessage: errors.array()
+      errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password,
+        confirmPassword: req.body.confirmPassword
+      },
+      validationErrors: errors.array()
     });
   }
-  User.findOne({email: email})
-  .then(userDoc => {
-    if(userDoc) {
-      req.flash('error','Email already exist!');
-      return res.redirect('/signup')
-    }
-    return bcrypt
+     bcrypt
     .hash(password, 12)
     .then(hashedPassword => {
       const user = new User({
@@ -116,8 +160,7 @@ exports.postSignup = (req, res, next) => {
     })
     .catch(err => {
       console.log(err);
-    });
-  })
+    })
   .catch(err => {
     console.log(err);
   });
--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,64 @@
 const express = require('express');
-const { check } = require('express-validator/check');
+// const { check } = require('express-validator/check');
+const { check, body } = require('express-validator')
 
 const authController = require('../controllers/auth');
+const User = require('../models/user');
 
 const router = express.Router();
 
 router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
+    
+router.post('/login',[
+    body('email')
+        .isEmail()
+        .withMessage('Incorrect email.')
+        .normalizeEmail(),
+    body('password', 'Incorrect Password')
+        .isLength({min: 5})
+        .isAlphanumeric()
+        .trim()
+], 
+authController.postLogin
+);
 
-router.post('/login', authController.postLogin);
-
-router.post('/signup', check('email').isEmail(), authController.postSignup);
+router.post('/signup', [
+    check('email')
+    .isEmail()
+    .withMessage('Invalid Email')
+    .custom((value, {req}) => {
+        // if(value === 'test@gmail.com') {
+        //     throw new Error('Forbidden email address');
+        // }
+        // return true;
+        return User.findOne({email: value})
+        .then(userDoc => {
+            if(userDoc) {
+            return Promise.reject('Email already exist!');
+            }
+        });
+})
+.normalizeEmail(),
+body(
+    'password',
+    'Password should be numbers and text and atleast 5 characters.'
+)
+.isLength({min: 5})
+.isAlphanumeric()
+.trim(),
+body('confirmPassword')
+.trim()
+.custom((value, {req}) => {
+    if(value !== req.body.password) {
+        throw new Error('Password not matched');
+    }
+    return true;
+})
+],
+authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
